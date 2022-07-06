1e356a1060e33be0ecbfe22887f0b2a0bb3ff959
John Servo <jaservo@up.edu.ph>
2022-07-06 15:09:32 +0800

    added files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackplugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: '[name][ext]',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test:/\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackplugin({
+            title: 'Todo List',
+            filename: 'index.html',
+            template: './src/template.html',
+        })
+    ]
+}
