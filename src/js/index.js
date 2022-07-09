b44a0066993556d878c6d561306293ca9ceaafd8
John Servo <jaservo@up.edu.ph>
2022-07-09 18:23:40 +0800

    add add/exit todo

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 import '../styles/main.scss';
 
-console.log("YEAH");
+const addItemButton = document.getElementById("addButton");
+const addItemBox = document.getElementById("addItem");
+const addItemExit = document.getElementById("exit");
+const body = document.querySelector("body");
+const list = document.getElementById("list");
+
+addItemButton.addEventListener("click", e => {
+    addItemBox.style.display = "grid";
+    body.style.pointerEvents = "none";
+    list.style.overflow = "hidden";
+});
+
+addItemExit.addEventListener("click", e => {
+    addItemBox.style.display = "none";
+    body.style.pointerEvents = "all";
+    list.style.overflow = "scroll";
+})
